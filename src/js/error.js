491cc5887d9b874026663038d207d1ee46b68565
Sled Oleksandr <sled.oleksandr@gmail.com>
2021-06-06 14:38:20 +0200

    Поправляет показ алерта

--- src/js/error.js
+++ src/js/error.js
@@ -1,6 +1,9 @@
 export default function onFatchError(response, gallery) {
   if (!response.ok) {
     console.dir(response);
-    gallery.innerHTML = `<p class="error">Что-то пошло не так!</p>`;
+    gallery.insertAdjacentHTML(
+      'beforeend',
+      `<p class="error">Что-то пошло не так!</p>`,
+    );
   }
 }
--- src/js/setMurcup.js
+++ src/js/setMurcup.js
@@ -1,7 +1,10 @@
 export default async function makeMurcup(response, gallery, callback) {
   if (response.length === 0) {
-    gallery.innerHTML =
-      '<p class="allert">Внимание!</br>По ваему запросу ничего не найдено, уточните поиск.</p>';
+    gallery.insertAdjacentHTML(
+      'beforeend',
+      '<p class="allert">Внимание!</br>По ваему запросу ничего не найдено, уточните поиск.</p>',
+    );
+
     return;
   } else {
     gallery.insertAdjacentHTML('beforeend', callback);
