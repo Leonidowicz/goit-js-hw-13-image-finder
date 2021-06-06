b757b09fcdfa3c11ec7167f05ac0b615c60ac340
Sled Oleksandr <sled.oleksandr@gmail.com>
2021-06-06 14:24:13 +0200

    Исправляет недочеты(убирает кнопку когда меньше 12ти, запрос не летит на пробел, файлы в папках)

--- src/index.js
+++ src/index.js
@@ -1,28 +1,33 @@
-import './styles.scss';
-import onFetchError from './error';
+import './css/styles.scss';
+import onFetchError from './js/error';
 
-import ref from './ref';
-import fetchImages from './apiService';
+import ref from './js/ref';
+import fetchImages from './js/apiService';
 
-import getMarkup from './getMurcup.hbs';
-import setMurcup from './setMurcup';
+import getMarkup from './temlates/getMurcup.hbs';
+import setMurcup from './js/setMurcup';
 
-ref.form.addEventListener('submit', inputHandler);
+ref.form.addEventListener('submit', submitHandler);
 ref.more.addEventListener('click', showMore);
 let searchQuery = '';
 
-function inputHandler(event) {
+function submitHandler(event) {
   event.preventDefault();
   searchQuery = event.target.elements.input.value.trim();
+  if (searchQuery.length === 0) {
+    return;
+  }
 
   fetchImages(searchQuery)
     .then(response => {
-      ref.gallery.innerHTML = '';
-      setMurcup(response.hits, ref.gallery, getMarkup(response));
       console.log(response.hits.length);
+
       response.hits.length > 0
         ? ref.more.classList.remove('is-hidden')
         : ref.more.classList.add('is-hidden');
+      ref.gallery.innerHTML = '';
+
+      setMurcup(response.hits, ref.gallery, getMarkup(response));
     })
     .catch(error => onFetchError(error, ref.gallery))
     .finally((event.target.elements.input.value = ''));
@@ -31,8 +36,22 @@ function inputHandler(event) {
 function showMore() {
   fetchImages(searchQuery)
     .then(response => {
+      console.log(response.hits.length);
+
+      if (response.hits.length < 12) {
+        ref.more.classList.add('is-hidden');
+      }
+      console.dir(ref.more);
       setMurcup(response.hits, ref.gallery, getMarkup(response));
-      ref.more.scrollIntoView({ behavior: 'smooth', block: 'end' });
+      ref.gallery.lastElementChild.scrollIntoView({
+        behavior: 'smooth',
+        block: 'end',
+      });
+      ref.more.scrollIntoView({
+        behavior: 'smooth',
+        block: 'end',
+      });
     })
-    .catch(error => onFetchError(error, ref.gallery));
+    .catch(error => onFetchError(error, ref.gallery))
+    .finally();
 }
